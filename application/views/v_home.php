123f775288c003f6be7430cdf822739bf6aa3ebf
risa alfadila <risaalfadila29@gmail.com>
2022-07-22 18:40:24 +0700

    comit pertama

---
+++ application/views/v_home.php
@@ -0,0 +1,8 @@
+<div id="map" style="width:400px; height: 300px;"></div>
+<script>
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGFkYW5ndHV0b3JpYWwiLCJhIjoiY2s0emg1NmtpMGJ1MTN1bzU0YjkzNTh3diJ9.sNUWKUHnuFylosfT0nD1tQ';
+    var map = new mapboxgl.Map({
+        container :'map',
+        style : 'mapbox:://styles/mapbox/streets-V11'
+    });
+</script>
